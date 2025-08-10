9d1e6d63cd2fb302a60ffdc5812d3f2aff714a86
Pranish97 <99318459+Pranish97@users.noreply.github.com>
2025-08-10 22:11:13 +0545

    Setup Project

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+mongoose
+  .connect("mongodb+srv://pranishstha4:Pranish123@rms.otc8usw.mongodb.net/")
+  .then(() => console.log("Database Connected"))
+  .catch((error) => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST ", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log("Server Is Running"));
